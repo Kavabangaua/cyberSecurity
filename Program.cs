a00e49493b93c478f2d89547aa1fe05ff2fc85f3
Kavabangaua <125158622+Kavabangaua@users.noreply.github.com>
2023-09-11 14:29:48 +0300

    Release

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Random random1 = new Random(42); // Seed with value 42
+        Random random2 = new Random(100); // Seed with value 100
+
+        Console.WriteLine("Generating random numbers for random1:");
+        GenerateAndDisplayRandomNumbers(random1);
+
+        Console.WriteLine("\nGenerating random numbers for random2:");
+        GenerateAndDisplayRandomNumbers(random2);
+
+
+        CompareRandomSequences(random1, random2);
+
+        Console.ReadLine();
+    }
+
+    static void GenerateAndDisplayRandomNumbers(Random random)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(random.Next());
+        }
+    }
+
+    static void CompareRandomSequences(Random random1, Random random2)
+    {
+        Console.WriteLine("\nComparing random sequences for different initial values:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            int value1 = random1.Next();
+            int value2 = random2.Next();
+
+            Console.WriteLine($"Value from random1: {value1}, Value from random2: {value2}, Equal? {value1 == value2}");
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        int numTrials = 5;
+        int sequenceLength = 10;
+
+        for (int trial = 1; trial <= numTrials; trial++)
+        {
+            Console.WriteLine($"Trial {trial}:");
+
+            byte[] randomSequence1 = GenerateRandomSequence(sequenceLength);
+            byte[] randomSequence2 = GenerateRandomSequence(sequenceLength);
+
+            Console.WriteLine("Random Sequence 1: ");
+            DisplayByteArray(randomSequence1);
+
+            Console.WriteLine("Random Sequence 2: ");
+            DisplayByteArray(randomSequence2);
+
+            bool sequencesEqual = CompareByteArrays(randomSequence1, randomSequence2);
+
+            if (sequencesEqual)
+            {
+                Console.WriteLine("Sequences are equal.");
+            }
+            else
+            {
+                Console.WriteLine("Sequences are not equal.");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+
+    static byte[] GenerateRandomSequence(int length)
+    {
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            byte[] randomBytes = new byte[length];
+            rng.GetBytes(randomBytes);
+            return randomBytes;
+        }
+    }
+
+
+    static bool CompareByteArrays(byte[] arr1, byte[] arr2)
+    {
+        if (arr1.Length != arr2.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            if (arr1[i] != arr2[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    static void DisplayByteArray(byte[] arr)
+    {
+        foreach (byte b in arr)
+        {
+            Console.Write($"{b:X2} ");
+        }
+        Console.WriteLine();
+    }
+}
